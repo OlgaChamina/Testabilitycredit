05e9128e82d385213f62e80dc5bb6bfe57cf9826
OlgaCh <chamina51@mail.ru>
2023-11-01 23:07:31 +0300

    first commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,14 +1,12 @@
 public class CreditPaymentService {
     public int calculate(int credit, int term) {
-        int n = term*12;
+        int n = term * 12;
         double persent = 9.99;
-        double m = persent/12/100;
+        double m = persent / 12 / 100;
 
-        double annuity =  m * Math.pow((1 + m), n)/(Math.pow((1 + m), n) - 1);
-        double payment = credit*annuity;
+        double annuity = m * Math.pow((1 + m), n) / (Math.pow((1 + m), n) - 1);
+        double payment = credit * annuity;
         return (int) payment;
-
-
 
 
     }
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service= new CreditPaymentService();
+        CreditPaymentService service = new CreditPaymentService();
 
 
         System.out.println();
         System.out.println("срок кредита: 1 год / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
-        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,1));
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000, 1));
 
         System.out.println();
         System.out.println("срок кредита: 2 года / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
-        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,2));
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000, 2));
 
         System.out.println();
         System.out.println("срок кредита: 3 года / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
-        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,3));
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000, 3));
     }
 }

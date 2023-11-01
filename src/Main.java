98b500ca6884bff2ea660aee2d464bd3ac760688
OlgaCh <chamina51@mail.ru>
2023-11-01 22:31:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service= new CreditPaymentService();
+
+
+        System.out.println();
+        System.out.println("срок кредита: 1 год / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,1));
+
+        System.out.println();
+        System.out.println("срок кредита: 2 года / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,2));
+
+        System.out.println();
+        System.out.println("срок кредита: 3 года / сумма кредита: 1_000_000 р. / процентная ставка: 9,99%");
+        System.out.println("ежемесячный платеж: " + service.calculate(1_000_000,3));
+    }
+}
